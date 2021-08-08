30e29113da499a0891d69c564b335fa52e4ff25f
miltoskr <miltoskartalas@gmail.com>
2021-08-08 22:56:11 +0300

    moved .env to root directory

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const app = express();
 const connectDB = require('./config/db');
 const path = require('path');
 //Setup environment variables
-dotenv.config({
-	path: __dirname + 'config/.env',
-});
+// dotenv.config({
+//     path: __dirname + '../config/.env',
+// });
 
 //Connect to Database
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1999;
 
 //Use json body parser
 app.use(express.json());
 
 app.listen(PORT, () => {
-	console.log(`Server is up on port ${PORT}`);
+    console.log(`Server is up on port ${PORT}`);
 });
