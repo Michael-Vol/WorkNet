b0cffebe833d15c6d8db97cd746d2c75ca488fe8
MichaelVol <mivolakis@gmail.com>
2021-08-08 21:43:08 +0300

    added basic express and db condig

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+console.log(process.env.MONGO_URI);
+const mongoURI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(mongoURI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+
+		console.log('DB connected...');
+	} catch (error) {
+		console.error(error); //couldn't connect to DB
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
