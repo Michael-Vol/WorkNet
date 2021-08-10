e981e903a092aa980c410c2d09ced86abdf18166
miltoskr <miltoskartalas@gmail.com>
2021-08-10 19:11:56 +0300

    test comment

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,22 +3,22 @@ const { Mongoose } = require('mongoose');
 const User = require('../models/User');
 const router = express.Router();
 
-router.post('/signup', async (req, res) => {
-	try {
-		const user = new User(req.body);
-		const token = user.generateAuthToken();
-		res.status(201).json({
-			user,
-			token,
-		});
+router.post('/signup', async(req, res) => {
+    try {
+        const user = new User(req.body);
+        const token = user.generateAuthToken();
+        res.status(201).json({
+            user,
+            token,
+        });
 
-		await user.save();
-	} catch (error) {
-		res.status(500).send(error);
-		res.status(500).json({
-			message: 'Server Error',
-		});
-	}
+        await user.save();
+    } catch (error) {
+        res.status(500).send(error);
+        res.status(500).json({
+            message: 'Server Error',
+        });
+    }
 });
-
+// test comment
 module.exports = router;
