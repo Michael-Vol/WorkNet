8167125c957885eed7bb153fe41ec7c84edd18f4
MichaelVol <mivolakis@gmail.com>
2021-08-10 18:54:52 +0300

    test commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,31 +3,23 @@ const { Mongoose } = require('mongoose');
 const User = require('../models/User');
 const router = express.Router();
 
-router.post('/signup', async(req, res) => {
+router.post('/signup', async (req, res) => {
+	try {
+		const user = new User(req.body);
+		// user.dateOfBirth = new Date(req.body.dateOfBirth)
+		const token = user.generateAuthToken();
+		res.status(201).json({
+			user,
+			token,
+		});
 
-    try {
-
-        const user = new User(req.body);
-        // user.dateOfBirth = new Date(req.body.dateOfBirth)
-        const token = user.generateAuthToken();
-        res.status(201).json({
-
-            user,
-            token
-        });
-        /* Unhandled promise pairnoume gia to save, epsaxa kai eida oti prepei na einai se try, catch block alla den doulepse. Se diko tou try and catch, an to baleis sto 
-        hdh uparxon den tha doulespei kan to save */
-        await user.save();
-
-    } catch (error) {
-
-        res.status(500).send(error)
-        res.status(500).json({
-            message: 'Server Error',
-        });
-    }
+		await user.save();
+	} catch (error) {
+		res.status(500).send(error);
+		res.status(500).json({
+			message: 'Server Error',
+		});
+	}
 });
-
-
 
 module.exports = router;
