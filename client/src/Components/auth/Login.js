29339b17fb7698b0fe59ab5ed62cb77b764eb89d
miltoskr <miltoskartalas@gmail.com>
2021-08-30 18:48:49 +0300

    fixed login form

--- client/src/Components/auth/Login.js
+++ client/src/Components/auth/Login.js
@@ -4,93 +4,108 @@ import { loginUser } from '../../Actions/auth';
 import toast, { Toaster } from 'react-hot-toast';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { Container, Form, FormGroup, FormControl, ControlLabel, Icon, Button, ButtonToolbar, Schema } from 'rsuite';
+import {
+    Container,
+    Form,
+    FormGroup,
+    FormControl,
+    ControlLabel,
+    Icon,
+    Button,
+    ButtonToolbar,
+    Schema,
+    FlexboxGrid,
+    Content,
+    Panel,
+} from 'rsuite';
 const Login = () => {
-	const [loginData, setLoginData] = useState({
-		email: '',
-		password: '',
-	});
-	const [formError, setFormError] = useState({});
+    const [loginData, setLoginData] = useState({
+        email: '',
+        password: '',
+    });
+    const [formError, setFormError] = useState({});
 
-	const formRef = useRef();
+    const formRef = useRef();
 
-	const { email, password } = loginData;
+    const { email, password } = loginData;
 
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	const { StringType } = Schema.Types;
-	const model = Schema.Model({
-		email: StringType().isRequired('This field is required.').isEmail('Please enter a valid email'),
-		password: StringType()
-			.isRequired('This field is required')
-			.rangeLength(6, 20, 'The number of characters in the password field must be between 6 and 20'),
-	});
+    const { StringType } = Schema.Types;
+    const model = Schema.Model({
+        email: StringType().isRequired('This field is required.').isEmail('Please enter a valid email'),
+        password: StringType()
+            .isRequired('This field is required')
+            .rangeLength(6, 20, 'The number of characters in the password field must be between 6 and 20'),
+    });
 
-	const handleSubmit = async () => {
-		if (!formRef.current.check()) {
-			toast.error('Cannot create Account. Check your form information.');
-		} else {
-			const res = await loginUser({ email, password });
-			const reducerRes = dispatch(res);
-			console.log(reducerRes);
-		}
-	};
+    const handleSubmit = async () => {
+        if (!formRef.current.check()) {
+            toast.error('Cannot create Account. Check your form information.');
+        } else {
+            const res = await loginUser({ email, password });
+            const reducerRes = dispatch(res);
+            console.log(reducerRes);
+        }
+    };
 
-	return (
-		<div>
-			<Container className='login--container'>
-				<Toaster position='top-right' toastOptions={{ duration: 4000 }} />
-				<section className='form--title'>
-					<i className='fas fa-sign-in-alt'></i>
-					<span className='form__title'>Login</span>
-				</section>
-
-				<Form
-					ref={formRef}
-					layout='horizontal'
-					model={model}
-					formValue={loginData}
-					onChange={(value) => setLoginData(value)}
-					onCheck={(formError) => setFormError(formError)}>
-					<FormGroup key='form__email'>
-						<ControlLabel className='form__label'>
-							<i className='fas fa-envelope form__icon'></i>
-							<span>Email</span>
-						</ControlLabel>
-						<FormControl name='email'></FormControl>
-					</FormGroup>
-
-					<FormGroup key='form__password'>
-						<ControlLabel className='form__label'>
-							<i className='fas fa-key form__icon'></i>
-							<span>Password</span>
-						</ControlLabel>
-						<FormControl name='password'></FormControl>
-					</FormGroup>
-
-					<FormGroup>
-						<ButtonToolbar>
-							<Button
-								className='form--submit-btn'
-								type='submit'
-								onClick={() => handleSubmit()}
-								appearance='primary'>
-								Login
-							</Button>
-							<Button
-								className='form--cancel-btn'
-								appearance='default'
-								onClick={() => {
-									return <Redirect to='/' />;
-								}}>
-								Cancel
-							</Button>
-						</ButtonToolbar>
-					</FormGroup>
-				</Form>
-			</Container>
-		</div>
-	);
+    return (
+        <div>
+            <Container className='login--container'>
+                <Toaster position='top-right' toastOptions={{ duration: 4000 }} />
+                <Content>
+                    <FlexboxGrid justify='center'>
+                        <FlexboxGrid.Item colspan={16}>
+                            <Panel header={<h3>Login</h3>} bordered>
+                                <Form
+                                    fluid
+                                    ref={formRef}
+                                    layout='horizontal'
+                                    model={model}
+                                    formValue={loginData}
+                                    onChange={(value) => setLoginData(value)}
+                                    onCheck={(formError) => setFormError(formError)}>
+                                    <FormGroup key='form__email'>
+                                        <ControlLabel className='form__label'>
+                                            <i className='fas fa-envelope form__icon'></i>
+                                            <span>Email</span>
+                                        </ControlLabel>
+                                        <FormControl name='email'></FormControl>
+                                    </FormGroup>
+                                    <FormGroup key='form__password'>
+                                        <ControlLabel className='form__label'>
+                                            <i className='fas fa-key form__icon'></i>
+                                            <span>Password</span>
+                                        </ControlLabel>
+                                        <FormControl name='password'></FormControl>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <ButtonToolbar>
+                                            <Button
+                                                // className='form--submit-btn'
+                                                type='submit'
+                                                onClick={() => handleSubmit()}
+                                                appearance='primary'>
+                                                Login
+                                            </Button>
+                                            <Button
+                                                // className='form--cancel-btn'
+                                                appearance='default'
+                                                onClick={() => {
+                                                    return <Redirect to='/' />;
+                                                }}>
+                                                Cancel
+                                            </Button>
+                                        </ButtonToolbar>
+                                    </FormGroup>
+                                </Form>
+                            </Panel>
+                        </FlexboxGrid.Item>
+                    </FlexboxGrid>
+                </Content>
+            </Container>
+        </div>
+    );
 };
 
 export default Login;
--- client/src/Components/auth/Register.js
+++ client/src/Components/auth/Register.js
@@ -5,183 +5,183 @@ import { useDispatch, useSelector } from 'react-redux';
 import { registerUser } from '../../Actions/auth';
 import { Redirect } from 'react-router-dom';
 import {
-	Container,
-	Uploader,
-	Form,
-	FormGroup,
-	FormControl,
-	ControlLabel,
-	HelpBlock,
-	Icon,
-	Button,
-	ButtonToolbar,
-	Schema,
-	Notification,
+    Container,
+    Uploader,
+    Form,
+    FormGroup,
+    FormControl,
+    ControlLabel,
+    HelpBlock,
+    Icon,
+    Button,
+    ButtonToolbar,
+    Schema,
+    Notification,
 } from 'rsuite';
 
 const Register = (props) => {
-	const [formData, setFormData] = useState({
-		name: '',
-		email: '',
-		password: '',
-		password2: '',
-		phone: '',
-	});
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+        phone: '',
+    });
 
-	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
-	const dispatch = useDispatch();
-	const [formError, setFormError] = useState({});
-	const [avatar, setAvatar] = useState({});
+    const dispatch = useDispatch();
+    const [formError, setFormError] = useState({});
+    const [avatar, setAvatar] = useState({});
 
-	const formRef = useRef();
+    const formRef = useRef();
 
-	if (isAuthenticated) {
-		return <Redirect to='/dashboard' />;
-	}
+    if (isAuthenticated) {
+        return <Redirect to='/dashboard' />;
+    }
 
-	const { name, email, password, password2, phone } = formData;
-	const { StringType } = Schema.Types;
+    const { name, email, password, password2, phone } = formData;
+    const { StringType } = Schema.Types;
 
-	const model = Schema.Model({
-		name: StringType().isRequired('This field is required.'),
-		email: StringType().isRequired('This field is required').isEmail('Please enter a valid email address.'),
-		password: StringType()
-			.isRequired('This field is required')
-			.rangeLength(6, 20, 'The number of characters in the password field must be between 6 and 20'),
-		password2: StringType()
-			.addRule((value, data) => {
-				if (value !== data.password) {
-					return false;
-				}
+    const model = Schema.Model({
+        name: StringType().isRequired('This field is required.'),
+        email: StringType().isRequired('This field is required').isEmail('Please enter a valid email address.'),
+        password: StringType()
+            .isRequired('This field is required')
+            .rangeLength(6, 20, 'The number of characters in the password field must be between 6 and 20'),
+        password2: StringType()
+            .addRule((value, data) => {
+                if (value !== data.password) {
+                    return false;
+                }
 
-				return true;
-			}, 'The two passwords do not match')
-			.isRequired('This field is required.'),
-		phone: StringType().isRequired('This field is required'),
-	});
+                return true;
+            }, 'The two passwords do not match')
+            .isRequired('This field is required.'),
+        phone: StringType().isRequired('This field is required'),
+    });
 
-	const handleSubmit = async () => {
-		console.log(formData);
+    const handleSubmit = async () => {
+        console.log(formData);
 
-		if (!formRef.current.check()) {
-			//open('error', 'Cannot create Account. Check your form information.');
-			toast.error('Cannot create Account. Check your form information.');
-		} else {
-			const [firstName, lastName] = name.split(' ');
-			const res = await registerUser({ firstName, lastName, email, password, phone, avatar });
-			dispatch(res);
-			toast.success('Account Created');
-		}
-	};
+        if (!formRef.current.check()) {
+            //open('error', 'Cannot create Account. Check your form information.');
+            toast.error('Cannot create Account. Check your form information.');
+        } else {
+            const [firstName, lastName] = name.split(' ');
+            const res = await registerUser({ firstName, lastName, email, password, phone, avatar });
+            dispatch(res);
+            toast.success('Account Created');
+        }
+    };
 
-	return (
-		<div>
-			<Container className='register--container'>
-				<Toaster position='top-right' toastOptions={{ duration: 4000 }} />
-				<section className='form--title'>
-					<i className=' fas fa-pencil-alt fa-lg'></i>
-					<span className='form__title'>Register</span>
-				</section>
+    return (
+        <div>
+            <Container className='register--container'>
+                <Toaster position='top-right' toastOptions={{ duration: 4000 }} />
+                <section className='form--title'>
+                    <i className=' fas fa-pencil-alt fa-lg'></i>
+                    <span className='form__title'>Register</span>
+                </section>
 
-				<Form
-					layout='horizontal'
-					ref={formRef}
-					model={model}
-					formValue={formData}
-					onChange={(value) => {
-						setFormData(value);
-					}}
-					onCheck={(formError) => {
-						setFormError(formError);
-					}}>
-					<FormGroup key='form__name'>
-						<ControlLabel className='form__label'>
-							<i className='fas fa-user form__icon'></i>
-							<span>Name</span>
-						</ControlLabel>
-						<FormControl name='name'></FormControl>
-						<HelpBlock>Required</HelpBlock>
-					</FormGroup>
+                <Form
+                    layout='horizontal'
+                    ref={formRef}
+                    model={model}
+                    formValue={formData}
+                    onChange={(value) => {
+                        setFormData(value);
+                    }}
+                    onCheck={(formError) => {
+                        setFormError(formError);
+                    }}>
+                    <FormGroup key='form__name'>
+                        <ControlLabel className='form__label'>
+                            <i className='fas fa-user form__icon'></i>
+                            <span>Name</span>
+                        </ControlLabel>
+                        <FormControl name='name'></FormControl>
+                        <HelpBlock>Required</HelpBlock>
+                    </FormGroup>
 
-					<FormGroup key='form__email'>
-						<ControlLabel className='form__label'>
-							<i className='fas fa-envelope form__icon'></i>
-							<span>Email</span>
-						</ControlLabel>{' '}
-						<FormControl name='email'></FormControl>
-						<HelpBlock>Required</HelpBlock>
-					</FormGroup>
-					<FormGroup key='form__password'>
-						<ControlLabel className='form__label'>
-							<i className='fas fa-key form__icon'></i>
-							<span>Password</span>
-						</ControlLabel>
-						<FormControl name='password'></FormControl>
-						<HelpBlock>Required</HelpBlock>
-					</FormGroup>
+                    <FormGroup key='form__email'>
+                        <ControlLabel className='form__label'>
+                            <i className='fas fa-envelope form__icon'></i>
+                            <span>Email</span>
+                        </ControlLabel>{' '}
+                        <FormControl name='email'></FormControl>
+                        <HelpBlock>Required</HelpBlock>
+                    </FormGroup>
+                    <FormGroup key='form__password'>
+                        <ControlLabel className='form__label'>
+                            <i className='fas fa-key form__icon'></i>
+                            <span>Password</span>
+                        </ControlLabel>
+                        <FormControl name='password'></FormControl>
+                        <HelpBlock>Required</HelpBlock>
+                    </FormGroup>
 
-					<FormGroup key='form__password2'>
-						<ControlLabel className='form__label'>
-							<i className='fas fa-key form__icon'></i>
-							<span>Verify Password</span>
-						</ControlLabel>
-						<FormControl name='password2'></FormControl>
-						<HelpBlock>Required</HelpBlock>
-					</FormGroup>
+                    <FormGroup key='form__password2'>
+                        <ControlLabel className='form__label'>
+                            <i className='fas fa-key form__icon'></i>
+                            <span>Verify Password</span>
+                        </ControlLabel>
+                        <FormControl name='password2'></FormControl>
+                        <HelpBlock>Required</HelpBlock>
+                    </FormGroup>
 
-					<FormGroup key='form__tel'>
-						<ControlLabel className='form__label'>
-							<i className='fas fa-phone form__icon'></i>
-							<span>Phone Number</span>
-						</ControlLabel>{' '}
-						<FormControl name='phone'></FormControl>
-						<HelpBlock>Required</HelpBlock>
-					</FormGroup>
+                    <FormGroup key='form__tel'>
+                        <ControlLabel className='form__label'>
+                            <i className='fas fa-phone form__icon'></i>
+                            <span>Phone Number</span>
+                        </ControlLabel>{' '}
+                        <FormControl name='phone'></FormControl>
+                        <HelpBlock>Required</HelpBlock>
+                    </FormGroup>
 
-					<FormGroup key='form__avatar'>
-						<ControlLabel className='form__label'>
-							<i className='fas fa-portrait form__icon'></i>
-							<span>Profile Photo</span>
-						</ControlLabel>
-						<Uploader
-							name='avatar'
-							autoUpload={false}
-							listType='picture'
-							multiple={false}
-							onChange={(fileList) => {
-								setAvatar(fileList[0]);
-							}}>
-							<button>
-								<Icon icon='camera-retro' size='lg' />
-							</button>
-						</Uploader>
-						{/* <HelpBlock>Required</HelpBlock> */}
-					</FormGroup>
+                    <FormGroup key='form__avatar'>
+                        <ControlLabel className='form__label'>
+                            <i className='fas fa-portrait form__icon'></i>
+                            <span>Profile Photo</span>
+                        </ControlLabel>
+                        <Uploader
+                            name='avatar'
+                            autoUpload={false}
+                            listType='picture'
+                            multiple={false}
+                            onChange={(fileList) => {
+                                setAvatar(fileList[0]);
+                            }}>
+                            <button>
+                                <Icon icon='camera-retro' size='lg' />
+                            </button>
+                        </Uploader>
+                        {/* <HelpBlock>Required</HelpBlock> */}
+                    </FormGroup>
 
-					<FormGroup>
-						<ButtonToolbar>
-							<Button
-								className='form--submit-btn'
-								type='submit'
-								onClick={() => handleSubmit()}
-								appearance='primary'>
-								Register
-							</Button>
-							<Button
-								className='form--cancel-btn'
-								appearance='default'
-								onClick={() => {
-									return <Redirect to='/' />;
-								}}>
-								Cancel
-							</Button>
-						</ButtonToolbar>
-					</FormGroup>
-				</Form>
-			</Container>
-		</div>
-	);
+                    <FormGroup>
+                        <ButtonToolbar>
+                            <Button
+                                className='form--submit-btn'
+                                type='submit'
+                                onClick={() => handleSubmit()}
+                                appearance='primary'>
+                                Register
+                            </Button>
+                            <Button
+                                className='form--cancel-btn'
+                                appearance='default'
+                                onClick={() => {
+                                    return <Redirect to='/' />;
+                                }}>
+                                Cancel
+                            </Button>
+                        </ButtonToolbar>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
+    );
 };
 
 export default Register;
