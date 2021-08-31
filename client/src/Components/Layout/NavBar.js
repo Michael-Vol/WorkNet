41519d9a3469e408b6261f689d0ca83e803a0fd1
Michael-Vol <mivolakis@gmail.com>
2021-09-01 00:02:25 +0300

    added navbar authlinks and guestlinks

--- client/src/Components/Layout/NavBar.js
+++ client/src/Components/Layout/NavBar.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Navbar, Nav, FlexboxGrid } from 'rsuite';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './NavBar.scss';
 const NavBar = ({ onSelect, activeKey, ...props }) => {
+	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+	const guestLinks = (
+		<div>
+			<Nav.Item href='/login' className='link'>
+				Login
+			</Nav.Item>
+			<Nav.Item href='/register' className='link'>
+				Register
+			</Nav.Item>
+		</div>
+	);
+	const authLinks = (
+		<div>
+			<Nav.Item href='/dashboard' className='link'>
+				Dashboard
+			</Nav.Item>
+			<Nav.Item href='/network' className='link'>
+				Network
+			</Nav.Item>
+			<Nav.Item href='/jobs' className='link'>
+				Jobs
+			</Nav.Item>
+			<Nav.Item href='/chats' className='link'>
+				Chats
+			</Nav.Item>
+			<Nav.Item href='/notifications' className='link'>
+				Notifications
+			</Nav.Item>
+			<Nav.Item href='/personal-info' className='link'>
+				Personal Info
+			</Nav.Item>
+			<Nav.Item href='/personal-info' className='link'>
+				Settings
+			</Nav.Item>
+		</div>
+	);
 	return (
 		<div className='navbar__container'>
 			<Navbar {...props} appearance='inverse'>
@@ -14,12 +51,7 @@ const NavBar = ({ onSelect, activeKey, ...props }) => {
 				<Navbar.Body>
 					<FlexboxGrid justify='end'>
 						<Nav onSelect={onSelect} activeKey={activeKey}>
-							<Nav.Item href='/login' className='link'>
-								Login
-							</Nav.Item>
-							<Nav.Item href='/register' className='link'>
-								Register
-							</Nav.Item>
+							<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
 						</Nav>
 					</FlexboxGrid>
 				</Navbar.Body>
