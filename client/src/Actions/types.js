65ebda43961f9342b149176f49a179a34b866d23
Michael-Vol <mivolakis@gmail.com>
2021-09-01 21:04:21 +0300

    created get personal info action and reducer

--- client/src/Actions/types.js
+++ client/src/Actions/types.js
@@ -4,3 +4,5 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
+export const GET_PERSONAL_INFO = 'GET_PERSONAL_INFO';
+export const PERSONAL_INFO_ERROR = 'PERSONAL_INFO_ERROR';
