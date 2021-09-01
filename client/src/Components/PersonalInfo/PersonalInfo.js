6c209bfbe509c397710c75901ef6eea6b7a2ac68
miltoskr <miltoskartalas@gmail.com>
2021-09-01 16:46:35 +0300

    fixed sidenav in personalinfo

--- client/src/Components/PersonalInfo/PersonalInfo.js
+++ client/src/Components/PersonalInfo/PersonalInfo.js
@@ -3,28 +3,31 @@ import './PersonalInfo.scss';
 import { Sidenav, Icon, Sidebar, Container, Nav, Dropdown, FlexboxGrid } from 'rsuite';
 
 const PersonalInfo = () => {
-	return (
-		<Container className='info--sidebar'>
-			<FlexboxGrid justify='start'>
-				<FlexboxGrid.Item colspan={8}>
-					<Sidebar className='info--sidebar'>
-						<Sidenav activeKey='1' className='sidenav--container' appearance='inverse'>
-							<Sidenav.Body>
-								<Nav>
-									<Nav.Item eventKey='1' active icon={<Icon icon='dashboard' />}>
-										Dashboard
-									</Nav.Item>
-									<Nav.Item eventKey='2' icon={<Icon icon='group' />}>
-										User Group
-									</Nav.Item>
-								</Nav>
-							</Sidenav.Body>
-						</Sidenav>
-					</Sidebar>
-				</FlexboxGrid.Item>
-			</FlexboxGrid>
-		</Container>
-	);
+    return (
+        <Container>
+            <FlexboxGrid justify='start'>
+                <FlexboxGrid.Item colspan={3}>
+                    {/* <Sidebar className='info--sidebar'> */}
+                    <Sidenav activeKey='1' className='sidenav--container' appearance='inverse'>
+                        <Sidenav.Body>
+                            <Nav>
+                                <Nav.Item eventKey='1' active icon={<Icon icon='dashboard' />}>
+                                    Work Experience
+                                </Nav.Item>
+                                <Nav.Item eventKey='2' icon={<Icon icon='group' />}>
+                                    Education
+                                </Nav.Item>
+                                <Nav.Item eventKey='3' icon={<Icon icon='group' />}>
+                                    Skills
+                                </Nav.Item>
+                            </Nav>
+                        </Sidenav.Body>
+                    </Sidenav>
+                    {/* </Sidebar> */}
+                </FlexboxGrid.Item>
+            </FlexboxGrid>
+        </Container>
+    );
 };
 
 export default PersonalInfo;
