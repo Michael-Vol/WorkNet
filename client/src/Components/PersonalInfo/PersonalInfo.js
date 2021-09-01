b373bea7188a822db823939ffdf077379077555e
Michael-Vol <mivolakis@gmail.com>
2021-09-01 14:53:15 +0300

    started working on personal-info sidebar

--- client/src/Components/PersonalInfo/PersonalInfo.js
+++ client/src/Components/PersonalInfo/PersonalInfo.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import './PersonalInfo.scss';
-import { Sidenav, Icon, Sidebar, Container, Nav, Dropdown } from 'rsuite';
+import { Sidenav, Icon, Sidebar, Container, Nav, Dropdown, FlexboxGrid } from 'rsuite';
 
 const PersonalInfo = () => {
 	return (
-		<div className='show-fake-browser sidebar-page'>
-			<Container className>
-				<Sidebar className='info--sidebar'>
-					<Sidenav activeKey='1' className='sidenav--container'>
-						<Sidenav.Body>
-							<Nav>
-								<Nav.Item eventKey='1' active icon={<Icon icon='dashboard' />}>
-									Dashboard
-								</Nav.Item>
-								<Nav.Item eventKey='2' icon={<Icon icon='group' />}>
-									User Group
-								</Nav.Item>
-							</Nav>
-						</Sidenav.Body>
-					</Sidenav>
-				</Sidebar>
-			</Container>
-		</div>
+		<Container className='info--sidebar'>
+			<FlexboxGrid justify='start'>
+				<FlexboxGrid.Item colspan={8}>
+					<Sidebar className='info--sidebar'>
+						<Sidenav activeKey='1' className='sidenav--container' appearance='inverse'>
+							<Sidenav.Body>
+								<Nav>
+									<Nav.Item eventKey='1' active icon={<Icon icon='dashboard' />}>
+										Dashboard
+									</Nav.Item>
+									<Nav.Item eventKey='2' icon={<Icon icon='group' />}>
+										User Group
+									</Nav.Item>
+								</Nav>
+							</Sidenav.Body>
+						</Sidenav>
+					</Sidebar>
+				</FlexboxGrid.Item>
+			</FlexboxGrid>
+		</Container>
 	);
 };
 
