0f32b4eed1bbc447f7e7b32ba5733d5a5c3e7e9b
Michael-Vol <mivolakis@gmail.com>
2021-09-01 20:47:53 +0300

    added get user personal-info route

--- client/src/Components/PersonalInfo/PersonalInfo.js
+++ client/src/Components/PersonalInfo/PersonalInfo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './PersonalInfo.scss';
-import { Sidenav, Icon, Sidebar, PanelGroup, Container, Panel, Col, Row, Nav, Dropdown, FlexboxGrid } from 'rsuite';
+import { Sidenav, Icon, Sidebar, PanelGroup, Container, Panel, Col, Row, Nav, Dropdown, FlexboxGrid, Button } from 'rsuite';
 import PersonalInfoItem from './PersonalInfoItem';
 import PersonalInfoSideNav from './PersonalInfoSideNav';
 const PersonalInfo = () => {
@@ -13,15 +13,14 @@ const PersonalInfo = () => {
 				<FlexboxGrid.Item colspan={20}>
 					<PanelGroup>
 						<FlexboxGrid justify='space-around'>
+							<Button>Add Work Experience</Button>
 							<PersonalInfoItem
 								headerName='header1'
-								text='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quae amet quod unde ipsum perferendis natus iure nostrum cupiditate soluta!
-'
+								text='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quae amet quod unde ipsum perferendis natus iure nostrum cupiditate soluta!'
 							/>
 							<PersonalInfoItem
 								headerName='header1'
-								text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, magni neque. Optio soluta voluptatum vel itaque omnis atque eaque amet quod veniam accusantium qui temporibus iste voluptatibus ipsam laboriosam recusandae aspernatur, nulla repellat impedit quasi voluptas totam repudiandae inventore ratione? Quisquam animi cum omnis ipsam, excepturi eveniet laborum provident modi.
-'
+								text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, magni neque. Optio soluta voluptatum vel itaque omnis atque eaque amet quod veniam accusantium qui temporibus iste voluptatibus ipsam laboriosam recusandae aspernatur, nulla repellat impedit quasi voluptas totam repudiandae inventore ratione? Quisquam animi cum omnis ipsam, excepturi eveniet laborum provident modi.'
 							/>
 							<PersonalInfoItem
 								headerName='header1'
