cb076915f090bd70178579472ee0d65db1fc8588
Michael-Vol <mivolakis@gmail.com>
2021-09-01 22:58:10 +0300

    added add experience modal

--- client/src/Components/PersonalInfo/PersonalInfo.js
+++ client/src/Components/PersonalInfo/PersonalInfo.js
@@ -1,20 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './PersonalInfo.scss';
-import {
-	Sidenav,
-	Icon,
-	Sidebar,
-	PanelGroup,
-	Container,
-	Input,
-	Panel,
-	Col,
-	Row,
-	Nav,
-	Dropdown,
-	FlexboxGrid,
-	Button,
-} from 'rsuite';
+import { PanelGroup, Container, Row, FlexboxGrid, Form, FormGroup, FormControl, ControlLabel, Button, Modal } from 'rsuite';
 import PersonalInfoItem from './PersonalInfoItem';
 import PersonalInfoSideNav from './PersonalInfoSideNav';
 import { getPersonalInfo } from '../../Actions/personalInfo';
@@ -43,13 +29,36 @@ const PersonalInfo = () => {
 						<Button className='add-item-btn' onClick={() => setAddExperience(!addExperience)}>
 							Add Experience
 						</Button>
-						{addExperience && (
-							<Row xs={12}>
-								<Col>
-									<Input componentClass='textarea' rows={3} placeholder='Description' />;
-								</Col>
-							</Row>
-						)}
+						<Modal show={addExperience}>
+							<Modal.Header>Add Experience</Modal.Header>
+							<Modal.Body>
+								<h3>Work Experience</h3>
+								<Form fluid layout='horizontal'>
+									<FormGroup key='form__email'>
+										<ControlLabel className='form__label'>
+											<i className='fas fa-briefcase form__icon'></i>
+											<span>Work Name</span>
+										</ControlLabel>
+										<FormControl name='name'></FormControl>
+									</FormGroup>
+									<FormGroup key='form__description'>
+										<ControlLabel className='form__label'>
+											<i className='fas fa-edit form__icon'></i>
+											<span>Description</span>
+										</ControlLabel>
+										<FormControl componentClass='textarea' name='password'></FormControl>
+									</FormGroup>
+								</Form>
+							</Modal.Body>
+							<Modal.Footer>
+								<Button appearance='primary' onClick={() => setAddExperience(false)}>
+									Ok
+								</Button>
+								<Button appearance='subtle' onClick={() => setAddExperience(false)}>
+									Cancel
+								</Button>
+							</Modal.Footer>
+						</Modal>
 						<Row>
 							{personalInfo.workExperience &&
 								personalInfo.workExperience.map((work) => {
